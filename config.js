643292f214cbe5a2a9494161f92c3cba5e60b129
clien/xcfv/nearby <139028351+Newsajik@users.noreply.github.com>
2024-10-11 16:59:49 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   blocked: {
     // Normal users can ignore this option. This is for the owner of the website.
-    "https://interstellar-remake.vercel.app": "",
+    "gointerstellar.app": "",
     localhost: "",
   },
   users: {
